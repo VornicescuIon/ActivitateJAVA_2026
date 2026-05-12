e4a36434a300bb3751ada533e59238885ff27982
ion <vornicescuion23@stud.ase.ro>
2026-05-12 10:19:26 +0300

    incercare

---
+++ src/main/java/org/example/Client_1.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.*;
+import java.net.*;
+
+public class Client_1 {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 5001);
+
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream())
+        );
+
+        out.println("Buna ziua!");
+
+        String response = in.readLine();
+        System.out.println(response);
+
+        socket.close();
+    }
+}
